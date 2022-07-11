c733016006c618b0398a6ec012f1e47d7545160c
Toshika Raghuwanshi <raghuwanshi.toshika17@gmail.com>
2022-07-11 22:19:41 +1000

    babel added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ const fruits = {
     kiwi: 5,
     pawPaw: 3,
     orange: 9,
-    mango: 4
+    mango: 5
 
 }
+
+const winterFruits ={
+    ...fruits,
+    custardApple: 5,
+}
 console.log(fruits)
+console.log(winterFruits)
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports ={
+    mode: "development",
+
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:"babel-loader",
+                }
+            }
+        ]
+    },
+
+    devServer:{
+        static:"./dist",
+    },
+};
